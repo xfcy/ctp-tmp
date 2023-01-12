1b37626ee02deea54423dd08d297a67fe990ada5
GitHub Action <action@github.com>
2023-01-12 07:27:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/xfcy/ctptmp/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xfcy/ctptmp/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xfcy.ctptmp.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xfcy.ctptmp.MyBundle
 
 class MyApplicationService {
 
